4449c052552029253775b0de023b87a6935ad734
miguel-quero <m.quero.2021@alumnos.urjc.es>
2023-01-09 16:30:14 +0100

    comentarios finales

--- src/dirname.js
+++ src/dirname.js
@@ -1,4 +1,5 @@
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
+//se exporta el nombre de la dirección URL
 export const __dirname = dirname(fileURLToPath(import.meta.url));
